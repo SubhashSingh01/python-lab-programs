2d5f0be8dccece03ec036d3ca9456bf0fe9c832b
Subhash Singh <132035631+SubhashSingh01@users.noreply.github.com>
2023-06-06 15:33:33 +0530

    email verification program

---
+++ emailverify.py
@@ -0,0 +1,35 @@
+#to verify an email
+email=input("enter email")
+j,k,d=0,0,0
+if len(email)>=6:
+    if email[0].isalpha():
+        if("@" in email)and(email.count("@")==1):
+            if(email[-4]==".")^(email[-3]=="."):
+                for i in email:
+                    if i.isspace():
+                        k=1
+                    elif(i.isalpha()):
+                        if(i.isupper()):
+                         j=1
+                    elif(i.isdigit):
+                        continue
+                    elif(i=="_" or i=="." or i=="@"):
+                        continue
+                    else:
+                        d=1
+                if k==1 or j==1 or d==1:
+                    print("wrong email 5")
+                else:
+                    print("right email")
+            else:
+                print("wrong email 4")
+        else:
+            print("wrong email 3")
+    else:
+        print("wrong email 2")
+else:
+    print("wrong email 1")
+            
+                
+                    
+        
